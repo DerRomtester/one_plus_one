043b5b05bb3c7e3830bb18db20ea6900ef8a5f43
DerRomtester <dieb.stefan.96@gmail.com>
2015-03-02 14:09:37 +0100

    make.sh : update
    Conflicts:
    	make.sh

--- make.sh
+++ make.sh
@@ -25,7 +25,7 @@ echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 if [ $# -gt 0 ]; then
 echo $1 > .version
 fi
-make -j8
+
 ../ramdisk_one_plus_one/dtbToolCM -2 -o ../ramdisk_one_plus_one/split_img/boot.img-dtb -s 2048 -p ../one_plus_one/scripts/dtc/ ../one_plus_one/arch/arm/boot/
 cp arch/arm/boot/zImage ../ramdisk_one_plus_one/split_img/boot.img-zImage
 cd ../ramdisk_one_plus_one/
