9c823ed2459f195b3c0f6f3b170d9a86797d08ae
stefan <dieb.stefan.96@gmail.com>
2015-03-30 13:33:42 +0200

    ramdisk build: update
    Signed-off-by: stefan <dieb.stefan.96@gmail.com>

--- ramdisk.sh
+++ ramdisk.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+echo "Cleaning old files"
+rm -f ../one_plus_one/zip/Tyr*
+rm -f ../one_plus_one/zip/boot.img
+rm -f ../ram*/image-new*
+rm -f ../ram*/ramdisk-new.cpio*
+rm -f ../ram*/spl*/boot.img-dtb
+rm -f ../ram*/spl*/boot.img-zImage
 ../ramdisk_one_plus_one/dtbToolCM -2 -o ../ramdisk_one_plus_one/split_img/boot.img-dtb -s 2048 -p ../one_plus_one/scripts/dtc/ ../one_plus_one/arch/arm/boot/
 cp arch/arm/boot/zImage ../ramdisk_one_plus_one/split_img/boot.img-zImage
 cd ../ramdisk_one_plus_one/
