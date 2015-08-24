0ce1f25712e567dc2566c92c1bbf7d3d1a9d50ec
DerRomtester <dieb.stefan.96@gmail.com>
2015-08-24 10:48:57 +0200

    msm_thermal: update tot use adac sensor
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -16,18 +16,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/msm_tsens.h>
-#include <linux/workqueue.h>
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
-#include <linux/msm_tsens.h>
+#include <linux/qpnp/qpnp-adc.h>
 #include <linux/msm_thermal.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
 
-unsigned int temp_threshold = 60;
+/* throttle CPU when temperature reaches 44°C*/
+unsigned int temp_threshold = 44;
 module_param(temp_threshold, int, 0644);
 
+/* check every 0.5 seconds for the CPU temperature */
 unsigned int temp_scan_interval = 500;
 module_param(temp_scan_interval, int, 0644);
 
@@ -58,7 +58,9 @@ enum threshold_levels {
 	LEVEL_HOT		= 1 << 2,
 };
 
-static struct msm_thermal_data msm_thermal_info;
+struct qpnp_vadc_chip *vadc_dev;
+
+enum qpnp_vadc_channels adc_chan;
 
 static struct delayed_work check_temp_work;
 
@@ -110,17 +112,18 @@ static void limit_cpu_freqs(uint32_t max_freq)
 
 static void check_temp(struct work_struct *work)
 {
-	struct tsens_device tsens_dev;
+	struct qpnp_vadc_result result;
 	uint32_t freq = 0;
-	long temp = 0;
+	int64_t temp;
 
-	tsens_dev.sensor_num = 0;
-	tsens_get_temp(&tsens_dev, &temp);
+	qpnp_vadc_read(vadc_dev, adc_chan, &result);
+	temp = result.physical;
 
-	if (temp_threshold >= 75)
-		temp_threshold = 75;
-	else if (temp_threshold <= 40)
-		temp_threshold = 40;
+	/* Limit temp threshold to a max value to prevent users damaging their phone*/
+	if (temp_threshold >= 55)
+	{
+		temp_threshold = 55;
+	}
 
 	if (info.throttling)
 	{
@@ -155,25 +158,21 @@ reschedule:
 
 static int msm_thermal_dev_probe(struct platform_device *pdev)
 {
-	int ret = 0;
-	struct device_node *node = pdev->dev.of_node;
-	struct msm_thermal_data data;
+	struct device_node *np = pdev->dev.of_node;
+	int ret;
 
-	memset(&data, 0, sizeof(struct msm_thermal_data));
+	vadc_dev = qpnp_get_vadc(&pdev->dev, "thermal");
 
-	ret = of_property_read_u32(node, "qcom,sensor-id", &data.sensor_id);
-	if (ret)
+	ret = of_property_read_u32(np, "qcom,adc-channel", &adc_chan);
+	if (ret) {
 		return ret;
-
-	WARN_ON(data.sensor_id >= TSENS_MAX_SENSORS);
-
-        memcpy(&msm_thermal_info, &data, sizeof(struct msm_thermal_data));
-
-        INIT_DELAYED_WORK(&check_temp_work, check_temp);
-        schedule_delayed_work_on(0, &check_temp_work, 5);
+	}
 
 	cpufreq_register_notifier(&msm_thermal_cpufreq_notifier,
 			CPUFREQ_POLICY_NOTIFIER);
+
+	INIT_DELAYED_WORK(&check_temp_work, check_temp);
+        schedule_delayed_work_on(0, &check_temp_work, 5);
 
 	return ret;
 }
@@ -186,7 +185,7 @@ static int msm_thermal_dev_remove(struct platform_device *pdev)
 }
 
 static struct of_device_id msm_thermal_match_table[] = {
-	{.compatible = "qcom,msm-thermal"},
+	{.compatible = "qcom,msm-thermal-simple"},
 	{},
 };
 
@@ -194,7 +193,7 @@ static struct platform_driver msm_thermal_device_driver = {
 	.probe = msm_thermal_dev_probe,
 	.remove = msm_thermal_dev_remove,
 	.driver = {
-		.name = "msm-thermal",
+		.name = "msm-thermal-simple",
 		.owner = THIS_MODULE,
 		.of_match_table = msm_thermal_match_table,
 	},
